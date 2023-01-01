7d1c632475143e3dd2cbd3a2286b22a8ea2ea55d
mina-henen <58582362+mina-henen@users.noreply.github.com>
2023-01-01 20:03:49 +0200

    Add files via upload

---
+++ project/src/matrix.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class matrix {
+    public static int[][] generate(int rows, int columns) {
+        int[][] mat = new int[rows][columns];
+        Random random = new Random();
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
+                mat[i][j] = random.nextInt(100) * 10;
+        return mat;
+    }
+    public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+        int resultRows = matrix1.length;
+        int resultColumns = matrix2[0].length;
+        int[][] result = new int[resultRows][resultColumns];
+        int columns2 = matrix2[0].length;
+        for (int i = 0; i < resultRows; i++) {
+            for (int j = 0; j < columns2; j++) {
+                result[i][j] = 0;
+                for (int k = 0; k < resultColumns; k++) {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        return result;
+    }
+}
