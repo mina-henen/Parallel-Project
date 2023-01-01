ae1ce126dc8f9684bb5f25e6fbf06bc755d41a02
mina-henen <mon.messi6@gmail.com>
2023-01-02 00:19:45 +0200

    using average

--- src/MatrixMultiplication.java
+++ src/MatrixMultiplication.java
@@ -4,20 +4,28 @@ import java.util.Date;
 public class MatrixMultiplication {
     public static void main(String[] args) {
         DecimalFormat df = new DecimalFormat("0.000");
-        for (int i = 100; i < 1001; i+=50) {
+        for (int i = 50; i < 1001; i+=50) {
             int[][] m1 = matrix.generate(i, i);
             int[][] m2 = matrix.generate(i, i);
-            Date serialStart = new Date();
-            int[][] serialResult = matrix.multiply(m1, m2);
-            Date serialEnd = new Date();
-            long sTime = (serialEnd.getTime() - serialStart.getTime());
+            long sTime=0;
+            for (int j = 0; j < 20; j++) {
+                Date serialStart = new Date();
+                int[][] serialResult = matrix.multiply(m1, m2);
+                Date serialEnd = new Date();
+                sTime += (serialEnd.getTime() - serialStart.getTime());
+            }
+            sTime/=20;
             System.out.println("For size = "+i);
             System.out.println("Serial Multiply Time taken in milli seconds: " + sTime);
             int[][] parallelResult = new int[m1.length][m2[0].length];
-            Date parallelStart = new Date();
-            ParallelThreadsCreator.multiply(m1, m2, parallelResult);
-            Date parallelEnd = new Date();
-            long pTime=(parallelEnd.getTime() - parallelStart.getTime());
+            long pTime=0;
+            for (int j = 0; j < 20; j++) {
+                Date parallelStart = new Date();
+                ParallelThreadsCreator.multiply(m1, m2, parallelResult);
+                Date parallelEnd = new Date();
+                pTime+=(parallelEnd.getTime() - parallelStart.getTime());
+            }
+            pTime/=20;
             System.out.println("Parallel Multiply Time taken in milli seconds: " + pTime);
             double speedUp=Double.parseDouble(df.format((double)sTime/pTime));
             System.out.println("Speed up = " + speedUp);
